b0fb75894364ccd782f9fbb7973d966148e2fb29
Dmitriy <ulrht88@gmail.com>
2024-11-19 12:55:32 +0300

    fix: add fix

--- config.py
+++ config.py
@@ -4,10 +4,8 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-"""
-Константы приложения.
-"""
 
+# Константы приложения
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 REDIRECT_URI = os.getenv("REDIRECT_URI")
@@ -16,7 +14,6 @@ REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 EMAIL = os.getenv("EMAIL")
 RESUME_ID = os.getenv("RESUME_ID")
 APPLICATION = os.getenv("APPLICATION")
-
 HEADERS = {
     "Authorization": f"Bearer {TOKEN}",
     "User-Agent": f"{APPLICATION} ({EMAIL})",
